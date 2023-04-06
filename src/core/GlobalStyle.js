0079147bacc14a7d731d91a7490127d57c12e628
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-06 16:10:40 +0200

    Added globalStyles

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+  
+  body {
+    font-family: 'Poppins', sans-serif;
+    word-break: break-word;
+    text-decoration: none;
+    background: ${({ theme }) => theme.color.mainColor};
+    color: ${({ theme }) => theme.color.fontPrimary};
+    
+    }
+  `;
---
+++ src/core/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+    color: {
+    },
+    breakPoint: {
+    },
+}
+
+export const boxShadow = "0px 0px 10px rgb(24, 24, 27)";
