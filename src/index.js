4531e5b6b83aaecc2821dfb8e6c5b012953a7622
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-07 12:14:03 +0200

    Added redux to app

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import App from './core/App';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './core/GlobalStyle';
 import { theme } from './core/theme';
+import { Provider } from 'react-redux';
+import { store } from './core/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
