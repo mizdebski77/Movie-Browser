e5b63c305f2b83e5bdc1e70fcbb6c36538f64b99
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-07 12:14:49 +0200

    Added colors to theme

--- src/core/theme.js
+++ src/core/theme.js
@@ -1,5 +1,10 @@
 export const theme = {
     color: {
+        mainColor: "rgb(245, 245, 250)",
+        secondColor: "rgb(24, 24, 27)",
+        fontPrimary: "#e3a207",
+        fontSecondary: "#ffff",
+
     },
     breakPoint: {
     },
