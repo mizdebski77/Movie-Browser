b96268035e6bf73ca3d807e2e4525aa7e3a72907
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-11 22:53:37 +0200

    Sth

--- src/common/Buttons/buttons.js
+++ src/common/Buttons/buttons.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { Arrows, Button, Page, Wrapper } from './styledButtons';
 import rightArrow from '../Images/rightArrow.svg';
 import leftArrow from '../Images/leftArrow.svg'
+import { useSelector } from 'react-redux';
+import { selectPage, selectTotalPages } from '../../features/Movies/MovieCard/FetchPopularMovies/moviesSlice';
 
 export const Buttons = () => {
+    const pageNumber = useSelector(selectPage);
+    const pagesNumber = useSelector(selectTotalPages);
+
     return (
         <Wrapper>
             <Button><Arrows src={leftArrow} />First</Button>
             <Button><Arrows src={leftArrow} />Previous</Button>
-            <Page>Page 1 of 500</Page>
-            <Button>Next<Arrows src={rightArrow} /> </Button>
+            <Page>Page {pageNumber} of {pagesNumber}</Page>
+            <Button >Next<Arrows src={rightArrow} /> </Button>
             <Button>Last<Arrows src={rightArrow} /> </Button>
         </Wrapper>
     );
--- src/features/Movies/MovieCard/FetchPopularMovies/moviesSlice.js
+++ src/features/Movies/MovieCard/FetchPopularMovies/moviesSlice.js
@@ -4,11 +4,12 @@ const moviesSlice = createSlice({
     name: "movies",
     initialState: {
         status: "loading",
+        pageNumber: 1,
         movies: [],
     },
 
     reducers: {
-        fetchMovies: (state) => {
+        fetchMovies: (state, ) => {
             state.status = "loading";
         },
 
@@ -20,13 +21,16 @@ const moviesSlice = createSlice({
         fetchMoviesError: (state) => {
             state.status = "error";
         },
+
     }
 });
 
-export const { fetchMovies, fetchMoviesError, fetchMoviesSuccess } = moviesSlice.actions;
+export const { fetchMovies, fetchMoviesError, fetchMoviesSuccess, currentPage } = moviesSlice.actions;
 export const selectMoviesState = state => state.movies;
 
 export const selectMovies = state => selectMoviesState(state).movies;
 export const selectStatus = state => selectMoviesState(state).status;
+export const selectPage = state => selectMoviesState(state).movies.page;
+export const selectTotalPages = state => selectMoviesState(state).movies.total_pages;
 
 export default moviesSlice.reducer;
