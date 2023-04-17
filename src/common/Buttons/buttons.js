13319adf946aeddfadee27096c5b5a8086f2dd5d
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-17 21:51:06 +0200

    Added scroll to button

--- src/common/Buttons/buttons.js
+++ src/common/Buttons/buttons.js
@@ -17,10 +17,15 @@ export const Buttons = () => {
     return (
         <Wrapper>
             <Button disabled={currentPage === 1} onClick={() => dispatch(setPage(1)) && window.scrollTo(0, 0)}><Arrows src={leftArrow} />First</Button>
-            <Button disabled={currentPage === 1} onClick={() => dispatch(setPage(currentPage -1)) && window.scrollTo(0, 0)}><Arrows src={leftArrow} />Previous</Button>
+            <Button disabled={currentPage === 1} onClick={() => dispatch(setPage(currentPage - 1)) && window.scrollTo(0, 0)}><Arrows src={leftArrow} />Previous</Button>
             <Page>Page {currentPage} of {lastPageNumber}</Page>
             <Button disabled={currentPage === lastPageNumber} onClick={() => dispatch(setPage(currentPage + 1)) && window.scrollTo(0, 0)}>Next<Arrows src={rightArrow} /></Button>
-            <Button disabled={currentPage === lastPageNumber} onClick={() => dispatch(setPage(lastPageNumber)) && window.scrollTo(0, 0) }>Last<Arrows src={rightArrow} /></Button>
+            <Button disabled={currentPage === lastPageNumber} onClick={() => dispatch(setPage(lastPageNumber)) && window.scrollTo(0, 0)}>Last<Arrows src={rightArrow} /></Button>
+            <Button scrollTo onClick={() => window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            })}> Scroll to top
+            </Button>
         </Wrapper >
     );
 };
--- src/common/Buttons/styledButtons.js
+++ src/common/Buttons/styledButtons.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import SVG from "react-inlinesvg";
 import { lighten } from 'polished';
 
@@ -8,6 +8,7 @@ export const Wrapper = styled.section`
     align-items: center;
     padding: 10px;
     gap: 10px;
+    margin-left: 105px;
 
 `;
 
@@ -17,7 +18,7 @@ export const Button = styled.button`
     align-items: center;
     justify-content: center;
     border: none;
-    background: ${({theme}) => theme.color.fontPrimary};
+    background: ${({ theme }) => theme.color.fontPrimary};
     padding: 8px 16px;
     min-width: 80px;
     border-radius: 5px;
@@ -26,20 +27,29 @@ export const Button = styled.button`
     
     &:hover {
         transform: scale(1.05);
-        background: ${({theme}) => lighten(0.1, theme.color.fontPrimary)};
+        background: ${({ theme }) => lighten(0.1, theme.color.fontPrimary)};
     }
     
     &:disabled {
         filter: opacity(0.7);
-        background: ${({theme}) => theme.color.fontPrimary};
+        background: ${({ theme }) => theme.color.fontPrimary};
         cursor: no-drop;
         transform: none;
     }
+
+    ${({ scrollTo }) => scrollTo && css`
+        color:${({ theme }) => theme.color.fontPrimary};
+        background: ${({theme}) => theme.color.secondColor};
+
+        &:hover {
+            background: ${({ theme }) => lighten(0.1, theme.color.secondColor)};
+        }
+    `};
 `;
 
 export const Page = styled.h3`
     margin: 0 10px 0 10px;
-    color: ${({theme}) => theme.color.secondColor};
+    color: ${({ theme }) => theme.color.secondColor};
     font-weight: normal;
 `;
 
