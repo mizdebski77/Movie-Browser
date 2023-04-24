76226a663e689190e87143139d635ee13ca5d074
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-24 16:08:36 +0200

    Removed "getMovieByID"

--- src/features/Movies/MovieCard/FetchPopularMovies/moviesSlice.js
+++ src/features/Movies/MovieCard/FetchPopularMovies/moviesSlice.js
@@ -35,13 +35,7 @@ export const selectMovies = state => selectMoviesState(state).movies.results;
 export const selectStatus = state => selectMoviesState(state).status;
 export const selectTotalPages = state => selectMoviesState(state).movies.total_pages;
 export const selectPage = state => selectMoviesState(state).page;
+export const selectTotalResuults = state => selectMoviesState(state).movies.total_results
 
-export const getDetailsByID = (state, movieID) => {
-    const movies = selectMovies(state);
-    if (movies && movies.length > 0) {
-        return movies.find(movie => movie.id === parseInt(movieID));
-    }
-    return undefined;
-};
 
 export default moviesSlice.reducer;
