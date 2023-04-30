d5e14f885284d1c409a5a901c8686f9097b2ff29
Marcin Izdebski <mizdebski123@gmail.com>
2023-04-30 18:10:00 +0200

    Added queryParameters

---
+++ src/core/queryParameters.js
@@ -0,0 +1,22 @@
+import { useLocation, useSearchParams } from "react-router-dom";
+
+export const useQueryParameter = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  return searchParams.get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  
+  return ({ key, value }) => {
+    if (!value) {
+      searchParams.delete(key);
+    } else {
+      searchParams.set(key, value);
+    };
+    setSearchParams(searchParams);
+  };
+};
+
+export const searchQueryParamName = "search";
